15985849edf1d467e81711b1482944cb64f00c37
Marcin Partyka <marpar12@o2.pl>
2026-05-02 20:58:27 +0200

    ConsoleUI zaczete, dodany plik z opisem ciekawych nuansow, Main utworzony

--- src/main/java/todo/Main.java
+++ src/main/java/todo/Main.java
@@ -1,4 +1,12 @@
 package todo;
 
+import todo.service.TaskManager;
+import todo.ui.ConsoleUI;
+
 public class Main {
+    public static void main(String[] args) {
+        TaskManager manager = new TaskManager();
+        ConsoleUI ui = new ConsoleUI(manager);
+        ui.start();
+    }
 }
--- src/main/java/todo/ui/ConsoleUI.java
+++ src/main/java/todo/ui/ConsoleUI.java
@@ -1,4 +1,64 @@
 package todo.ui;
 
+import todo.model.Priority;
+import todo.service.TaskManager;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    private TaskManager taskManager;
+    private Scanner scanner = new Scanner(System.in);
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+
+    public ConsoleUI(TaskManager taskManager){
+        this.taskManager = taskManager;
+    }
+
+    public void start(){
+        System.out.println(BLUE + "╔══════════╗" + RESET);
+        System.out.println(BLUE + "║  MENU    ║" + RESET);
+        System.out.println(BLUE + "╚══════════╝" + RESET);
+
+        System.out.println(BLUE + "┌─── ZADANIA ─────┐" + RESET);
+
+        System.out.println(BLUE + "┌─── MENU ────────┐" + RESET);
+        System.out.println(BLUE + "│  [1] Dodaj zadanie      [2] Ukończ zadanie │" + RESET);
+        System.out.println(BLUE + "│  [3] Usuń zadanie     │" + RESET);
+
+        while (true){
+            int wybor = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (wybor){
+                case 0: {
+                    return;
+                }
+                case 1: {
+                    System.out.println("Podaj nazwę zadania:");
+                    String nazwa = scanner.nextLine();
+                    System.out.println("Podaj ważność (LOW/MEDIUM/HIGH):");
+                    Priority waznosc = Priority.valueOf(scanner.nextLine().toUpperCase());
+                    taskManager.addTask(nazwa, waznosc);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Podaj id zadania: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+                    taskManager.completeTask(id);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Podaj id zadania: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+                    taskManager.deleteTask(id);
+                    break;
+                }
+            }
+        }
+    }
 }
