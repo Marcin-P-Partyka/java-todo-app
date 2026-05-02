ee9c0d7088b62bd8a3d2ab5e8df2142b6e38f188
Marcin Partyka <marpar12@o2.pl>
2026-05-02 20:59:44 +0200

    ConsoleUI zaczete, dodany plik z opisem ciekawych nuansow, Main utworzony

--- src/main/java/todo/Main.java
+++ src/main/java/todo/Main.java
@@ -4,7 +4,7 @@ import todo.service.TaskManager;
 import todo.ui.ConsoleUI;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         TaskManager manager = new TaskManager();
         ConsoleUI ui = new ConsoleUI(manager);
         ui.start();
